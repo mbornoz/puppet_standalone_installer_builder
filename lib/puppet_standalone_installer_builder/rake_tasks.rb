58003213249af6f34e582303f573e67c17436396
Christian Kaenzig <christian.kaenzig@camptocamp.com>
2014-07-17 14:18:15 +0200

    Add install.sh that installs and runs puppet

--- lib/puppet_standalone_installer_builder/rake_tasks.rb
+++ lib/puppet_standalone_installer_builder/rake_tasks.rb
@@ -64,13 +64,16 @@ task :build_tarball => [:build_check, :reprepro, :spec_prep, :spec_standalone] d
   properties = File.file?('.psib.yaml') ?  YAML.load_file('.psib.yaml') : {}
   properties['profile'] ||= profile
   properties['title'] ||= properties['profile']
-  template = ERB.new File.new(File.expand_path('../../../templates/ENDUSER.md.erb', __FILE__)).read, nil, "%"
-  File.open('spec/fixtures/ENDUSER.md', 'w') { |file| file.write(template.result(binding)) }
+  endusermd_template = ERB.new File.new(File.expand_path('../../../templates/ENDUSER.md.erb', __FILE__)).read, nil, "%"
+  File.open('spec/fixtures/ENDUSER.md', 'w') { |file| file.write(endusermd_template.result(binding)) }
+  installsh_template = ERB.new File.new(File.expand_path('../../../templates/install.sh.erb', __FILE__)).read, nil, "%"
+  Dir.mkdir('spec/fixtures/bin') unless File.exists?('spec/fixtures/bin')
+  File.open('spec/fixtures/bin/install.sh', 'w') { |file| file.write(installsh_template.result(binding)) }
 
   readme   = 'README.md' if File.file?('README.md')
   packages = 'packages' if File.exist?('packages')
 
-  sh "tar cvzfh #{tarball} #{readme} #{packages} --exclude-from .gitignore --exclude .git --exclude #{apt_dir}/conf --exclude #{apt_dir}/lists --exclude #{apt_dir}/db -C spec/fixtures ENDUSER.md manifests/ modules/ --exclude modules/#{profile}/spec/fixtures/modules --exclude modules/#{profile}/packages --transform 's,^,#{base_path}/,'"
+  sh "tar cvzfh #{tarball} #{readme} #{packages} --exclude-from .gitignore --exclude .git --exclude #{apt_dir}/conf --exclude #{apt_dir}/lists --exclude #{apt_dir}/db -C spec/fixtures ENDUSER.md bin/ manifests/ modules/ --exclude modules/#{profile}/spec/fixtures/modules --exclude modules/#{profile}/packages --transform 's,^,#{base_path}/,'"
 
   puts "Tarball of module #{profile} built in #{tarball}."
 end

c7a2bd62e859382c1313f8e0a4bb30ff4881630b
Raphaël Pinson <raphael.pinson@camptocamp.com>
2014-10-30 10:27:44 +0100

    Add docversion

--- pandoc.sh
+++ pandoc.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 FILE=$(readlink -f $1)
+VERSION=$2
+
+cat <<EOF > tex/docversion.tex
+\newcommand{\docversion}{$VERSION}
+EOF
 
 cd tex/ && pandoc -o ${FILE%.*}.pdf $FILE \
   --latex-engine=xelatex  --toc -H "header-includes.tex" \
